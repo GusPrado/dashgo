0f37ae5b1af6cc2fb2e2d3c0266b2bb2b982c5bc
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2022-07-12 21:19:28 -0300

    style: Change padding on create page for mobile

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -10,7 +10,7 @@ import {
 } from '@chakra-ui/react';
 import { Input } from '../../components/Form/Input';
 import { Header } from '../../components/Header';
-import { Sidebar } from '../../components/Sidebar/Sidebar';
+import { Sidebar } from '../../components/Sidebar';
 
 export default function UserCreate() {
   return (
@@ -20,19 +20,19 @@ export default function UserCreate() {
       <Flex w='100%' my='6' maxWidth={1480} mx='auto' px='6'>
         <Sidebar />
 
-        <Box flex='1' borderRadius={8} bg='gray.800' p='8'>
+        <Box flex='1' borderRadius={8} bg='gray.800' p={['4', '8']}>
           <Heading size='lg' fontWeight='normal'>
             Criar usuário
           </Heading>
           <Divider my='6' borderColor='gray.700' />
 
           <VStack spacing='8'>
-            <SimpleGrid minChildWidth='240px' spacing='8' w='100%'>
+            <SimpleGrid minChildWidth='240px' spacing={['4', '8']} w='100%'>
               <Input name='name' label='Nome completo' />
               <Input name='email' type='email' label='E-mail' />
             </SimpleGrid>
 
-            <SimpleGrid minChildWidth='240px' spacing='8' w='100%'>
+            <SimpleGrid minChildWidth='240px' spacing={['4', '8']} w='100%'>
               <Input name='password' type='password' label='Senha' />
               <Input
                 name='password_confirmation'
