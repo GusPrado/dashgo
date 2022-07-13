1a02094cb2f9a4698cf24ea0870b854525d9d021
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2022-07-13 15:39:27 -0300

    feat: Add App navigation flow

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -1,6 +1,7 @@
+import Link from 'next/link';
 import {
   Icon,
-  Link,
+  Link as ChackraLink,
   Text,
   LinkProps as ChakraLinkProps,
 } from '@chakra-ui/react';
@@ -9,14 +10,17 @@ import { ElementType } from 'react';
 interface NavLinkProps extends ChakraLinkProps {
   children: string;
   icon: ElementType;
+  href: string;
 }
-export function NavLink({ children, icon, ...rest }: NavLinkProps) {
+export function NavLink({ children, icon, href, ...rest }: NavLinkProps) {
   return (
-    <Link display='flex' alignItems='center' py='1' {...rest}>
-      <Icon as={icon} fontSize='20' />
-      <Text ml='4' fontWeight='medium'>
-        {children}
-      </Text>
+    <Link href={href} passHref>
+      <ChackraLink display='flex' alignItems='center' py='1' {...rest}>
+        <Icon as={icon} fontSize='20' />
+        <Text ml='4' fontWeight='medium'>
+          {children}
+        </Text>
+      </ChackraLink>
     </Link>
   );
 }
