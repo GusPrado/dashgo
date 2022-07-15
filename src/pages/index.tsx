ccb778fda764df0c9c609369f227321348141422
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2022-07-14 23:00:42 -0300

    feat: Add SignIn form functionality

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,19 @@
+import { SubmitHandler, useForm } from 'react-hook-form';
 import { Flex, Button, Stack } from '@chakra-ui/react';
-
 import { Input } from '../components/Form/Input';
 
+type SignInFormData = {
+  email: string;
+  password: string;
+};
+
 export default function SignIn() {
+  const { register, handleSubmit, formState } = useForm();
+
+  const handleSignIn: SubmitHandler<SignInFormData> = async (formValues) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+  };
+
   return (
     <Flex w='100vw' h='100vh' align='center' justify='center'>
       <Flex
@@ -13,12 +24,29 @@ export default function SignIn() {
         p='8'
         borderRadius={8}
         flexDir='column'
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing='4'>
-          <Input name='email' label='E-mail' type='email' />
-          <Input name='password' label='Senha' type='password' />
+          <Input
+            name='email'
+            label='E-mail'
+            type='email'
+            {...register('email')}
+          />
+          <Input
+            name='password'
+            label='Senha'
+            type='password'
+            {...register('password')}
+          />
 
-          <Button type='submit' mt='6' colorScheme='pink' size='lg'>
+          <Button
+            type='submit'
+            mt='6'
+            colorScheme='pink'
+            size='lg'
+            isLoading={formState.isSubmitting}
+          >
             Entrar
           </Button>
         </Stack>
