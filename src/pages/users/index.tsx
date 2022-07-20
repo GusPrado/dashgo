2801bb84de1277b36978e9a11db3e35a2eeeef94
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2022-07-19 22:35:46 -0300

    feat: Load users list thru React Query

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -16,7 +16,6 @@ import {
   useBreakpointValue,
 } from '@chakra-ui/react';
 import Link from 'next/link';
-import { useEffect } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 import { Header } from '../../components/Header';
@@ -27,7 +26,22 @@ export default function UserList() {
   const { isLoading, error, data } = useQuery(['users'], async () => {
     const response = await fetch('http://localhost:3000/api/users');
     const data = await response.json();
-    return data;
+
+    const users = data.users.map((user) => {
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric',
+        }),
+      };
+    });
+
+    console.log('usuarios', users);
+    return users;
   });
 
   const isWideVersion = useBreakpointValue({
@@ -82,31 +96,33 @@ export default function UserList() {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  <Tr>
-                    <Td px={['4', '4', '6']}>
-                      <Checkbox colorScheme='pink' />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text fontWeight='bold'>Gus Prado</Text>
-                        <Text fontSize='sm' color='gray.300'>
-                          gusgprado@gmail.com
-                        </Text>
-                      </Box>
-                    </Td>
-                    {isWideVersion && <Td>02 de Julho, 2022</Td>}
-                    <Td>
-                      <Button
-                        as='a'
-                        size='sm'
-                        fontSize='sm'
-                        colorScheme='purple'
-                        leftIcon={<Icon as={RiPencilLine} fontSize='16' />}
-                      >
-                        {isWideVersion ? 'Editar' : null}
-                      </Button>
-                    </Td>
-                  </Tr>
+                  {data.map((user) => (
+                    <Tr key={user.id}>
+                      <Td px={['4', '4', '6']}>
+                        <Checkbox colorScheme='pink' />
+                      </Td>
+                      <Td>
+                        <Box>
+                          <Text fontWeight='bold'>{user.name}</Text>
+                          <Text fontSize='sm' color='gray.300'>
+                            {user.email}
+                          </Text>
+                        </Box>
+                      </Td>
+                      {isWideVersion && <Td>{user.createdAt}</Td>}
+                      <Td>
+                        <Button
+                          as='a'
+                          size='sm'
+                          fontSize='sm'
+                          colorScheme='purple'
+                          leftIcon={<Icon as={RiPencilLine} fontSize='16' />}
+                        >
+                          {isWideVersion ? 'Editar' : null}
+                        </Button>
+                      </Td>
+                    </Tr>
+                  ))}
                 </Tbody>
               </Table>
 
